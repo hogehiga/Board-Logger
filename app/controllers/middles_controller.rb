e260bfd4ab5846761ff856925865530e4f9a4d17
Takahiro Iha <e165719@ie.u-ryukyu.ac.jp>
2018-12-04 14:20:41 +0900

    自分のボードだけを共有できるようになりました

--- app/controllers/middles_controller.rb
+++ app/controllers/middles_controller.rb
@@ -1,14 +1,24 @@
 class MiddlesController < ApplicationController
 
   def create
-    @middle = Middle.new(board_id: params[:board_id], user_id: params[:user_id])
-    @middle.save!
-    redirect_to root_path
+    if board
+      @middle = Middle.new(board_id: board.id, user_id: params[:user_id])
+      @middle.save!
+      redirect_to root_path
+    else
+      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+    end
   end
 
   def destroy
     @middle = Middle.find(params[:id])
     @middle.delete
     redirect_to root_path
+  end
+
+  private
+
+  def board
+    Board.find_by(user_id: current_user.id, id: params[:board_id])
   end
 end
