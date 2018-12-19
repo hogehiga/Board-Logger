603dee5d7bf3b795b62f1323c109d7b01318a9c9
Higashi Yonamine <e165714@ie.u-ryukyu.ac.jp>
2018-12-19 18:10:45 +0900

    showに統一しようとしているが，位置情報が取れない...

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,3 +1,4 @@
+/*
 window.onload = function geoFindMe() {
   var output = document.getElementById("out");
   if (!navigator.geolocation){
@@ -8,10 +9,13 @@ window.onload = function geoFindMe() {
   function success(position) {
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
-    document.getElementById("latitude").value = position.coords.latitude;
-    document.getElementById("longitude").value = position.coords.longitude;
+    //document.getElementById("latitude").value = position.coords.latitude;
+    //document.getElementById("longitude").value = position.coords.longitude;
+    document.getElementById("latitude").value = latitude;
+    document.getElementById("longitude").value = longitude;
+
     //ここで呼び出したい。
-    window.open("https://maps.google.com/maps?q=" + latitude +"," + longitude ,'_blank')
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
   }
 
   function error() {
@@ -21,4 +25,55 @@ debugger;
   output.innerHTML = "<p>Locating…</p>";
 
   navigator.geolocation.getCurrentPosition(success, error);
+}
+*/
+
+
+window.onload = function geoFindMe() {
+  var output = document.getElementById("out");
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    document.getElementById("latitude").value = latitude;
+    document.getElementById("longitude").value = longitude;
+    
+    //ここで呼び出したい。
+    //$.ajax(url: 'positions/create', type: "POST", data:{latitude,longitude});
+    //$.post("/positions/create", {data:1.6,authenticity_token: getCSRFtoken()})
+    //$.post("/boards/create",  {data:latitude,longitude,authenticity_token: getCSRFtoken()})
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+  }
+  function error() {
+    alert("エラー表示");
+    output.innerHTML = "Unable to retrieve your location";
+    errorCallback(error);
+  }
+  output.innerHTML = "<p>Locating…</p>";
+  navigator.geolocation.getCurrentPosition(success, error);
+  function errorCallback(error){
+    alert("エラー表示(エラーメッセージの中)");
+    var err_msg = "";
+    alert(error.code)
+    switch(error.code)
+    {
+      case 1:
+        alert("位置情報の利用が許可されていません");
+        //err_msg = "位置情報の利用が許可されていません";
+        break;
+      case 2:
+        alert("位置情報の利用が許可されていません");
+        err_msg = "デバイスの位置が判定できません";
+        break;
+      case 3:
+        alert("位置情報の利用が許可されていません");
+        err_msg = "タイムアウトしました";
+        break;
+      }
+    document.getElementById("out").innerHTML = err_msg;
+  }
 }
