3f1223a02bf8b4f44b964e47ca33feae5a422a3a
Higashi Yonamine <e165714@ie.u-ryukyu.ac.jp>
2018-12-21 03:28:02 +0900

    classで指定して,位置情報が保存もできる

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -11,16 +11,22 @@ window.onpageshow = function geoFindMe() {
     var longitude = position.coords.longitude;
     console.log(latitude);
     console.log(longitude)
-    //document.getElementById("latitude").value = latitude;
-    //document.getElementById("longitude").value = longitude;
-    document.getElementsByClassName("latitude").value = latitude;
-    document.getElementsByClassName("longitude").value = longitude;
+    if(output){//id = "out"がある時の処理
+      document.getElementById("board_latitude").value = latitude;//board/index.html.erbで，id指定しないと，"board_latitude"というidになっていた
+      document.getElementById("board_longitude").value = longitude;
+    }
+    //document.getElementsByClassName("latitude").value = latitude;
+    //document.getElementsByClassName("longitude").value = longitude;
     //ここで呼び出したい。
-    output.innerHTML = '<p>緯度は' + latitude + '° <br>経度は ' + longitude + '°</p>';
+    if(output){
+      output.innerHTML = '<p>緯度は' + latitude + '° <br>経度は ' + longitude + '°</p>';
+    }
   }
 
   function error(_error) {
-    output.innerHTML = "位置情報を測定できませんでした。";
+    if(output){
+      output.innerHTML = "位置情報を測定できませんでした。";
+    }
 
     switch(_error.code){
       case 1:
@@ -34,6 +40,8 @@ window.onpageshow = function geoFindMe() {
       break;
     }
   }
-  output.innerHTML = "<p>位置情報を測定中...</p>";
+  if(output){
+    output.innerHTML = "<p>位置情報を測定中...</p>";
+  }
   navigator.geolocation.getCurrentPosition(success, error);
 }
