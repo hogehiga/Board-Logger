1e192697091138874de3897c38afaca27037823b
hogehiga <hogehiga@gmail.com>
2018-12-28 18:56:19 +0900

    検索のテスト(ひとまず)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,26 @@ class UserTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "とりあえず完全一致で検索可能" do
+    user_1_name =users(:search_test_one).name
+    user_2_name =users(:search_test_two).name
+    k = User.search(user_2_name, user_1_name).count
+    assert_equal 1, k
+  end
+
+  test "検索で引っかかっても、自分自身は除外される" do
+    k = User.search('search_test', users(:search_test_one).name).count
+    assert_equal 1, k
+  end
+
+  test "空白で検索すると一件も引っかからない(現状のAPIだと呼び出し元が毎回nilチェックしないといけない)" do
+    assert_not User.search('', users(:search_test_one))
+  end
+
+  test "空白で検索すると一件も引っかからない(こっちのほうがnilチェック不要なので、呼び出し元にとっては扱いやすいAPIだと思う)" do
+    k = User.search('', users(:search_test_one).name).count
+    assert_equal 0, k
+  end
+
 end
